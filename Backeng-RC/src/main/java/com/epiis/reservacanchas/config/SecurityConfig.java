a301b5e6bd41d4a274cc12b6762a25a5b43ffaf3
gerardo1w <gerazero12345678@email.com>
2026-07-29 08:32:50 -0500

    plo

--- Backeng-RC/src/main/java/com/epiis/reservacanchas/config/SecurityConfig.java
+++ Backeng-RC/src/main/java/com/epiis/reservacanchas/config/SecurityConfig.java
@@ -28,7 +28,6 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors();
         http
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
