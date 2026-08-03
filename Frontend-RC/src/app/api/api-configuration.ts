5a47bc5435798618135820506d656fc6e9cc94ac
gerardo1w <gerazero12345678@email.com>
2026-08-03 12:52:19 -0500

    fix: refactorizar ApiConfiguration para consumir URL base dinamicamente

--- Frontend-RC/src/app/api/api-configuration.ts
+++ Frontend-RC/src/app/api/api-configuration.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiConfiguration {
-  rootUrl: string = 'http://localhost:8080/api';
+  rootUrl: string = environment.urlBase;
 }
